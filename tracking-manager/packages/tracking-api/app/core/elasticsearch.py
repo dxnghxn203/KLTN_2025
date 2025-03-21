3c3195ed8c14ccbd069e9786d48eb2b09a05458e
TuanNguyen238 <tuannguyen23823@gmail.com>
2025-03-22 01:58:09 +0700

    update nntuan

--- tracking-manager/packages/tracking-api/app/core/elasticsearch.py
+++ tracking-manager/packages/tracking-api/app/core/elasticsearch.py
@@ -5,14 +5,13 @@ from elasticsearch import Elasticsearch
 
 load_dotenv()
 
-es_host = os.getenv("ES_HOST")
-es_port = os.getenv("ES_PORT")
-es_user = os.getenv("ES_USER")
-es_pw = os.getenv("ES_PW")
-
-print(f"ES_HOST={es_host}, ES_PORT={es_port}, ES_USER={es_user}, ES_PW={es_pw}")
-
 try:
+    es_host = os.getenv("ES_HOST")
+    es_port = os.getenv("ES_PORT")
+    es_user = os.getenv("ES_USER")
+    es_pw = os.getenv("ES_PW")
+
+    print(f"ES_HOST={es_host}, ES_PORT={es_port}, ES_USER={es_user}, ES_PW={es_pw}")
     es_client = Elasticsearch(f"{es_host}:{es_port}", basic_auth=(es_user, es_pw), verify_certs=False)
     es_client.ping()
     print("Connected to Elasticsearch!")
