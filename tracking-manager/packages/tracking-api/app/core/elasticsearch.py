de27879ecf8c79835083ccbf4d40493160fd386d
TuanNguyen238 <tuannguyen23823@gmail.com>
2025-03-22 01:52:11 +0700

    updat enntuan

--- tracking-manager/packages/tracking-api/app/core/elasticsearch.py
+++ tracking-manager/packages/tracking-api/app/core/elasticsearch.py
@@ -10,9 +10,10 @@ es_port = os.getenv("ES_PORT")
 es_user = os.getenv("ES_USER")
 es_pw = os.getenv("ES_PW")
 
+print(f"ES_HOST={es_host}, ES_PORT={es_port}, ES_USER={es_user}, ES_PW={es_pw}")
+
 try:
-    es_client = Elasticsearch([f"{es_host}:{es_port}"], basic_auth=(es_user, es_pw))
-    print(f"es_client = {es_client}")
+    es_client = Elasticsearch(f"{es_host}:{es_port}", basic_auth=(es_user, es_pw), verify_certs=False)
     es_client.ping()
     print("Connected to Elasticsearch!")
 except Exception as e:
