4a5e06fa318b4f043268a3b70d9e0c0d4b828a44
TuanNguyen238 <tuannguyen23823@gmail.com>
2025-05-06 22:05:03 +0700

    update nntuan

--- tracking-manager/packages/tracking-api/app/routers/order.py
+++ tracking-manager/packages/tracking-api/app/routers/order.py
@@ -14,8 +14,8 @@ router = APIRouter()
 @router.post("/order/check_shipping_fee", response_model=response.BaseResponse)
 async def check_shipping_fee(item: ItemOrderInReq, session: str= None):
     try:
-        _, total_price, weight, out_of_stock_ids = await order.process_order_products(item.product)
-        if out_of_stock_ids:
+        _, total_price, weight, out_of_stock = await order.process_order_products(item.product)
+        if out_of_stock:
             return response.BaseResponse(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 message="Một số sản phẩm đã hết hàng",
