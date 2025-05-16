c3c8bad798847b38fe1d3fd58bc4b992e5f6fb94
Hxn203 <dxnghxn203@gmail.com>
2025-05-16 17:24:15 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  images: {
-    domains: [
-      "kltn2025.s3.ap-southeast-2.amazonaws.com",
-      "cdn.nhathuoclongchau.com.vn",
-    ],
-  },
+    reactStrictMode: false,
+    webpack: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+
+        // Thêm hỗ trợ alias paths
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': require('path').resolve(__dirname, 'src'),
+        };
+
+        return config;
+    },
+    images: {
+        domains: [
+            "kltn2025.s3.ap-southeast-2.amazonaws.com",
+            "cdn.nhathuoclongchau.com.vn",
+        ],
+    },
 };
 
 module.exports = nextConfig;
