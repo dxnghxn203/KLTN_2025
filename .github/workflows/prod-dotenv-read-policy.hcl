f645c45246dcbe8c4f44713e64f297d9502924c7
Hxn203 <dxnghxn203@gmail.com>
2025-05-18 16:29:27 +0700

    update

--- .github/workflows/prod-dotenv-read-policy.hcl
+++
@@ -1,3 +0,0 @@
-path "secret/data/production/.env" {
-  capabilities = ["read"]
-}
--- frontend/src/services/categoryService.ts
+++ frontend/src/services/categoryService.ts
@@ -1,16 +1,16 @@
 import axiosClient from "@/utils/configs/axiosClient";
-import { message } from "antd";
+import {message} from "antd";
 
-export const getAllCategory = async () : Promise<any>=> {
+export const getAllCategory = async (): Promise<any> => {
     try {
-        const response = await axiosClient.get("/v1/category");
+        const response = await axiosClient.get("/v1/category/");
         // console.log(response);
         return response;
     } catch (error) {
         throw error;
     }
 }
-export const getMainCategory = async (main_slug:any) : Promise<any>=> {
+export const getMainCategory = async (main_slug: any): Promise<any> => {
     try {
         const response = await axiosClient.get(`/v1/category/${main_slug}`);
         // console.log("service");
@@ -21,7 +21,7 @@ export const getMainCategory = async (main_slug:any) : Promise<any>=> {
     }
 }
 
-export const getSubCategory = async (main_slug:any, sub_slug:any) : Promise<any>=> {
+export const getSubCategory = async (main_slug: any, sub_slug: any): Promise<any> => {
     try {
         const response = await axiosClient.get(`/v1/category/${main_slug}/sub-category/${sub_slug}`);
         // console.log("service");
@@ -32,7 +32,7 @@ export const getSubCategory = async (main_slug:any, sub_slug:any) : Promise<any>
     }
 }
 
-export const getChildCategory = async (main_slug:any, sub_slug:any, child_slug:any) : Promise<any>=> {
+export const getChildCategory = async (main_slug: any, sub_slug: any, child_slug: any): Promise<any> => {
     try {
         const response = await axiosClient.get(`/v1/category/${main_slug}/sub-category/${sub_slug}/child-category/${child_slug}`);
 
@@ -43,7 +43,7 @@ export const getChildCategory = async (main_slug:any, sub_slug:any, child_slug:a
     }
 };
 
-export const getAllCategoryForAdmin = async () : Promise<any>=> {
+export const getAllCategoryForAdmin = async (): Promise<any> => {
     try {
         const response: any = await axiosClient.get("/v1/categories/get-all-for-admin");
         return {
@@ -59,7 +59,7 @@ export const getAllCategoryForAdmin = async () : Promise<any>=> {
     }
 };
 
-export const updateMainCategory = async (mainCategory: any, data: any) : Promise<any>=> {
+export const updateMainCategory = async (mainCategory: any, data: any): Promise<any> => {
     try {
         const params = `/v1/category/update/${mainCategory}?main_category_name=${data.main_category_name}&main_category_slug=${data.main_category_slug}`;
         const response: any = await axiosClient.put(params);
@@ -76,7 +76,7 @@ export const updateMainCategory = async (mainCategory: any, data: any) : Promise
     }
 }
 
-export const updateSubCategory = async (subCategoryId: any, data: any) : Promise<any>=> {
+export const updateSubCategory = async (subCategoryId: any, data: any): Promise<any> => {
     try {
         const params = `/v1/category/sub-category/update/${subCategoryId}?sub_category_name=${data.sub_category_name}&sub_category_slug=${data.sub_category_slug}`;
         const response: any = await axiosClient.put(params);
@@ -93,7 +93,7 @@ export const updateSubCategory = async (subCategoryId: any, data: any) : Promise
     }
 }
 
-export const updateChildCategory = async (childCategoryId: any, data: any) : Promise<any>=> {
+export const updateChildCategory = async (childCategoryId: any, data: any): Promise<any> => {
     try {
         const params = `/v1/category/child-category/update/${childCategoryId}?child_category_name=${data.child_category_name}&child_category_slug=${data.child_category_slug}`;
         const response: any = await axiosClient.put(params);
@@ -110,9 +110,9 @@ export const updateChildCategory = async (childCategoryId: any, data: any) : Pro
     }
 }
 
-export const addCategory = async (params: any) : Promise<any>=> {
+export const addCategory = async (params: any): Promise<any> => {
     try {
-        
+
         const response: any = await axiosClient.post("/v1/category/add", params);
         console.log("params", params);
         return {
@@ -129,117 +129,117 @@ export const addChildCategory = async (
     main_slug: string,
     sub_slug: string,
     body: {
-      child_category_name: string;
-      child_category_slug: string;
+        child_category_name: string;
+        child_category_slug: string;
     }
-  ): Promise<any> => {
+): Promise<any> => {
     try {
-      const url = `/v1/category/${main_slug}/sub-category/${sub_slug}/child-category/add`;
-  
-      const response: any = await axiosClient.post(url, body); // gửi JSON body
-  
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const url = `/v1/category/${main_slug}/sub-category/${sub_slug}/child-category/add`;
+
+        const response: any = await axiosClient.post(url, body); // gửi JSON body
+
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
-      throw error;
+        throw error;
     }
-  };
+};
 
 export const addSubCategory = async (
     main_slug: string, body: any): Promise<any> => {
     try {
-      const url = `/v1/category/${main_slug}/sub-category/add`;
-  
-      const response: any = await axiosClient.post(url, body); // gửi JSON body
-  
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
-    } catch (error: any) {
-      throw error;
-    }
-  }
+        const url = `/v1/category/${main_slug}/sub-category/add`;
 
-  export const updateImageSubCategory = async (
-    sub_category_id: string,
-    data: any
-    ): Promise<any> => {
-    try {
-      const params = `/v1/category/sub-category/${sub_category_id}/update-image`;
-      const response: any = await axiosClient.put(params, data);
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const response: any = await axiosClient.post(url, body); // gửi JSON body
+
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
         throw error;
-      }
     }
+}
+
+export const updateImageSubCategory = async (
+    sub_category_id: string,
+    data: any
+): Promise<any> => {
+    try {
+        const params = `/v1/category/sub-category/${sub_category_id}/update-image`;
+        const response: any = await axiosClient.put(params, data);
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
+    } catch (error: any) {
+        throw error;
+    }
+}
 
 export const updateImageChildCategory = async (
     child_category_id: string,
     data: any
-    ): Promise<any> => {
+): Promise<any> => {
     try {
-      const params = `/v1/category/child-category/${child_category_id}/update-image`;
-      const response: any = await axiosClient.put(params, data);
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const params = `/v1/category/child-category/${child_category_id}/update-image`;
+        const response: any = await axiosClient.put(params, data);
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
         throw error;
-      }
     }
+}
 
 export const deleteChildCategory = async (child_category_id: string): Promise<any> => {
     try {
-      const params = `/v1/category/child/${child_category_id}`;
-      const response: any = await axiosClient.delete(params);
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const params = `/v1/category/child/${child_category_id}`;
+        const response: any = await axiosClient.delete(params);
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
         throw error;
-      }
     }
+}
 
 export const deleteSubCategory = async (sub_category_id: string): Promise<any> => {
     try {
-      const params = `/v1/category/sub/${sub_category_id}`;
-      const response: any = await axiosClient.delete(params);
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const params = `/v1/category/sub/${sub_category_id}`;
+        const response: any = await axiosClient.delete(params);
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
         throw error;
-      }
     }
+}
 
 export const deleteMainCategory = async (main_category_id: string): Promise<any> => {
     try {
-      const params = `/v1/category/main/${main_category_id}`;
-      const response: any = await axiosClient.delete(params);
-      return {
-        status_code: response.status_code,
-        message: response.message,
-        data: response.data,
-      };
+        const params = `/v1/category/main/${main_category_id}`;
+        const response: any = await axiosClient.delete(params);
+        return {
+            status_code: response.status_code,
+            message: response.message,
+            data: response.data,
+        };
     } catch (error: any) {
         throw error;
-      }
     }
+}
 
   
 
