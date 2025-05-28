f02d892bd6da00261f069a8b879c16b20216e3ad
Hxn203 <dxnghxn203@gmail.com>
2025-05-28 14:02:09 +0700

    fix bug UI

--- frontend/src/components/Admin/Product/CreateProduct/dynamicReactQuill.tsx
+++ frontend/src/components/Admin/Product/CreateProduct/dynamicReactQuill.tsx
@@ -1,49 +1,51 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
+// import ReactQuill from "react-quill";
+
+// import "react-quill/dist/quill.snow.css";
 
 // Dynamic import với ssr: false và kiểu any
 const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false,
-  loading: () => (
-    <div className="border rounded-lg p-2 w-full h-32 bg-gray-50">
-      Đang tải editor...
-    </div>
-  ),
+    ssr: false,
+    loading: () => (
+        <div className="border rounded-lg p-2 w-full h-32 bg-gray-50">
+            Đang tải editor...
+        </div>
+    ),
 }) as any;
 
 export const toolbarOptions = [
-  [{ header: [1, 2, 3, 4, false] }],
-  [{ font: [] }],
-  [{ size: [] }],
-  ["bold", "italic", "underline", "strike", "blockquote"],
-  [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
-  [{ align: [] }],
-  ["link", "image", "video"],
-  ["clean"], // remove formatting button
+    [{header: [1, 2, 3, 4, false]}],
+    [{font: []}],
+    [{size: []}],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{list: "ordered"}, {list: "bullet"}, {indent: "-1"}, {indent: "+1"}],
+    [{align: []}],
+    ["link", "image", "video"],
+    ["clean"],
 ];
 
 interface DynamicQuillProps {
-  value: string;
-  onChange: (value: string) => void;
-  readOnly?: boolean;
+    value: string;
+    onChange: (value: string) => void;
+    readOnly?: boolean;
 }
 
 export default function DynamicReactQuill({
-  value,
-  onChange,
-  readOnly = false,
-}: DynamicQuillProps) {
-  return (
-    <div className="custom-quill">
-      <ReactQuill
-        value={value}
-        onChange={onChange}
-        modules={{ toolbar: toolbarOptions }}
-        readOnly={readOnly}
-        theme="snow"
-      />
-    </div>
-  );
+                                              value,
+                                              onChange,
+                                              readOnly = false,
+                                          }: DynamicQuillProps) {
+    return (
+        <div className="custom-quill">
+            <ReactQuill
+                value={value}
+                onChange={onChange}
+                modules={{toolbar: toolbarOptions}}
+                readOnly={readOnly}
+                theme="snow"
+            />
+        </div>
+    );
 }
