3be2a4e3eec43ec14c4601643effd479ddceb8a1
TuanNguyen238 <tuannguyen23823@gmail.com>
2025-06-05 02:07:05 +0700

    update nntuan

--- tracking-manager/packages/hook/api/update_status.go
+++ tracking-manager/packages/hook/api/update_status.go
@@ -11,36 +11,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var timeString = time.Now().Format(time.RFC3339Nano)
-
 type MedicareAPI struct {
 	statusQueue *rabbitmq.RabbitMQ
 }
 
 func (spx *MedicareAPI) UpdateStatus(c *gin.Context) {
+	bodyBytes, err := c.GetRawData()
+	if err != nil {
+		slog.Error("can't read raw body", "err", err, "time", time.Now().Format(time.RFC3339Nano))
+		c.JSON(http.StatusOK, gin.H{"result": false, "msg": "invalid body"})
+		return
+	}
+
+	slog.Info("receive raw body SC", "body", string(bodyBytes), "time", time.Now().Format(time.RFC3339Nano))
+
 	var model models.UpdateOrderStatusReq
 	if err := c.ShouldBindJSON(&model); err != nil {
 		slog.Error("can't parser req body err SC", "err", err)
-		c.JSON(http.StatusPreconditionFailed, gin.H{"result": false, "msg": err.Error()})
+		c.JSON(http.StatusOK, gin.H{"result": false, "msg": err.Error()})
 		return
 	}
 	j, _ := json.Marshal(model)
-	slog.Info("receive the msg SC", "mess", string(j), "time", timeString)
+	slog.Info("receive the msg SC", "mess", string(j), "time", time.Now().Format(time.RFC3339Nano))
 
 	modelMedicare, err := model.Mapping()
 	if err != nil {
-		slog.Error("push msg error SC", "error", err.Error(), "time", timeString)
-		c.JSON(http.StatusBadRequest, gin.H{"result": false, "msg": err.Error()})
+		slog.Error("push msg error SC", "error", err.Error(), "time", time.Now().Format(time.RFC3339Nano))
+		c.JSON(http.StatusOK, gin.H{"result": false, "msg": err.Error()})
 		return
 	}
 
 	if err := spx.statusQueue.Publish(modelMedicare); err != nil {
-		slog.Error("push msg error SC", "mess", err.Error(), "time", timeString)
-		c.JSON(http.StatusInternalServerError, gin.H{"result": false, "msg": "service unavailable"})
+		slog.Error("push msg error SC", "mess", err.Error(), "time", time.Now().Format(time.RFC3339Nano))
+		c.JSON(http.StatusOK, gin.H{"result": false, "msg": "service unavailable"})
 		return
 	}
 
 	j, _ = json.Marshal(modelMedicare)
-	slog.Info("push msg done SC", "mess", string(j), "time", timeString)
+	slog.Info("push msg done SC", "mess", string(j), "time", time.Now().Format(time.RFC3339Nano))
 	c.JSON(http.StatusOK, gin.H{"result": true, "msg": "Cập nhật trạng thái đơn hàng thành công"})
 }
